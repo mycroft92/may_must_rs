02969b386facb8e95f87c4afa60973383c3321f8
Madhukar yerraguntla <mycroft@Madhukars-MacBook-Air-2292.local>
2026-01-05 16:58:22 +0530

    redoing arragement

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,84 @@
-use llvm_sys::core::*;
-use llvm_sys::prelude::*;
-use llvm_sys::target::{LLVM_InitializeNativeAsmPrinter, LLVM_InitializeNativeTarget};
-use std::ffi::CStr;
-use std::ptr;
-pub unsafe fn count_instructions(module: LLVMModuleRef) {
-    let mut function = LLVMGetFirstFunction(module);
+mod errors;
+mod llvm_utils;
+use crate::expressions::exp::{parse_cmd_line, Assertion, Expr, Statement};
+use crate::llvm_utils::llvm_wrap::*;
+use clap::{arg, command, value_parser, ArgAction, Command};
+use env_logger::{Builder, Env};
+use log::*;
+use std::env;
+mod expressions;
 
-    while !function.is_null() {
-        // Get function name
-        let name = LLVMGetValueName2(function as LLVMValueRef, &mut 0);
-        let name = CStr::from_ptr(name).to_string_lossy();
-
-        let mut count = 0;
-        let mut block = LLVMGetFirstBasicBlock(function);
-
-        // Iterate through all basic blocks
-        while !block.is_null() {
-            let mut inst = LLVMGetFirstInstruction(block);
-
-            // Count instructions in this block
-            while !inst.is_null() {
-                count += 1;
-                inst = LLVMGetNextInstruction(inst);
-            }
-
-            block = LLVMGetNextBasicBlock(block);
+fn handle(module: Module) {
+    match llvm_utils::program_graph::generate_program_graph(&module) {
+        Ok(res_) => {
+            llvm_utils::program_graph::dump_graphs(&res_, "graph_dot");
         }
-
-        println!("Function {} has {} instructions", name, count);
-
-        function = LLVMGetNextFunction(function);
+        Err(err) => error!("{err}"),
     }
 }
 
-// Example usage:
-fn main() {
-    unsafe {
-        // Initialize LLVM
-        LLVM_InitializeNativeTarget();
-        LLVM_InitializeNativeAsmPrinter();
-
-        // Create module
-        let context = LLVMContextCreate();
-        let module =
-            LLVMModuleCreateWithNameInContext(b"my_module\0".as_ptr() as *const _, context);
-
-        // Run the pass
-        count_instructions(module);
-
-        // Cleanup
-        LLVMDisposeModule(module);
-        LLVMContextDispose(context);
+fn init_logger(level: &u8) {
+    let env;
+    match level {
+        0 => {
+            env = Env::default().filter_or("CRICK_LOG", "info");
+        }
+        _ => {
+            env = Env::default().filter_or("CRICK_LOG", "trace");
+        }
     }
-    print!("finished executing");
+    Builder::from_env(env).format_level(true).init();
+}
+
+fn main() {
+    let matches = command!() // requires `cargo` feature
+        .arg(arg!([name] "LLVM BC file to operate on"))
+        .arg(arg!(
+            -d --debug ... "Turn debugging information on"
+        ))
+        .arg(arg!(-a --assert <STRING> "assertion to look for"))
+        .get_matches();
+
+    let inpfile;
+    init_logger(
+        matches
+            .get_one::<u8>("debug")
+            .expect("Counts are defaulted"),
+    );
+    match matches.get_one::<String>("name") {
+        Some(name) => {
+            inpfile = name;
+        }
+        None => {
+            info!("Nothing to process");
+            return;
+        }
+    }
+    let assert_stmt;
+    match matches.get_one::<String>("assert") {
+        Some(cmd) => {
+            assert_stmt = cmd;
+        }
+        None => {
+            info!("Nothing to process");
+            return;
+        }
+    }
+
+    let assertion_ast;
+    match parse_cmd_line(assert_stmt) {
+        Err(e) => {
+            std::process::exit(1);
+        }
+        Ok(assertion) => {
+            assertion_ast = assertion;
+        }
+    }
+
+    initialize_target();
+    let context = Context::new();
+    match context.parse_bc_file(inpfile) {
+        Some(module) => handle(module),
+        None => {}
+    }
 }
